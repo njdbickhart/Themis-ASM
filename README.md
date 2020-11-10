aa04ef4a2d1fe9be218dd93bd778ebabe7a86a7a
Derek Bickhart <njdbickhart@gmail.com>
2020-11-10 17:26:53 -0600

    updates

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-# Themis
+# Themis-ASM
 ---
 
 A snakemake pipeline to compare assemblies with comparative alignment data. 
+
+## Table of Contents:
+
+## What is Themis-ASM?
+
+Named after the Greek Titaness of natural law, this workflow attempts to embody the aspect of [Themis](https://en.wikipedia.org/wiki/Themis) who was known as the "lady of good counsel." In practice, determining if your assembly is suitable for use by outside research groups often requires comparative analysis to identify potential flaws (or misassemblies). Themis-ASM is a workflow that combines several useful quality assessment tools for comparative analysis of assemblies against a reference version or even prior versions of the same assembly. 
+
+## What do I need to run Themis-ASM?
+
+Themis-ASM has several requirements for use. Let's first discuss the input datasets that are required prior to setting up the pipeline. We assume that you have at least two assemblies for your comparison and that you also have sequenced your reference/target sample with short-read sequence data. You will also need to select a [BUSCO](https://busco.ezlab.org/) database to align against your assembly for gene content statistics.
+
+#### Input file requirements:
+
+* At least two assembly fasta files 
+* At least one paired-end short-read sequence dataset (fastq format) to align against your assemblies
+* A BUSCO database label (ie. mammalia_odb10)
+
+If you do not have short-read data for your assemblies, it is possible to use publicly available data from online repositories such as the [SRA](https://www.ncbi.nlm.nih.gov/sra). The files must be paired-end at this time (there is no support for single-end reads... yet!).
+
+Now that we've detailed the dataset inputs for Themis-ASM, let's talk about software requirements. As we will touch on this in the next section, here is a brief listing of the requirements to run the pipeline:
+
+#### Software installation requirements:
+
+* Python 3.6+ installation
+	* Please make sure that Snakemake is installed and on your path!
+	* Also, please make sure that you have installed the following python packages:
+		* itertools
+		* pysam
+* [Merqury](https://genomebiology.biomedcentral.com/articles/10.1186/s13059-020-02134-9) must be on your path
+* [BWA v 0.7+](https://github.com/lh3/bwa) on your path
+* [Samtools v 1.9+](http://www.htslib.org/) on your path
+* [Miniconda or an equivalent "conda-based" package manager](https://docs.conda.io/en/latest/miniconda.html) on your path
+
+#### NOTE: Themis-ASM has only been tested on Linux-based systems! If you are trying to run the package on Windows, Mac or your smart toaster, we cannot provide support. 
+
+Themis-ASM can run on single computers or on distributed systems. Running tasks on clusters is accomplished through built-in job submission code within the Snakemake workflow system. Here are our required and recommended computational statistics for using the pipeline:
+
+#### Hardware requirements and recommendations:
+
+| Component  | Required | Recommended |
+| :--------- | :------- | :---------- |
+| RAM | 48 gigabytes | 300+ gigabytes recommended|
+| CPU threads | 8 threads |70+ recommended |
+| Storage per assembly| ~ 100 gigabytes | 200+ gigabytes |
+| Storage per short-read dataset | ~ 100 gigabytes | 200+ gigabytes |
+
+Hard disk footprints are estimated for genomes that are 1 gigabases in size and for short-read datasets that are 100 million reads of 150 x 150 paired end Illumina data, so please adjust these estimates accordingly based on your datasets! 
+
+## Running Themis-ASM for comparative assembly analysis
--- themisASM.py
+++ themisASM.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import os
+import logging
+import subprocess as sp
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG = logging.getLogger()
 
 version = "0.0.1"
 
 def parse_user_input():
     parser = argparse.ArgumentParser(
-            description = "Compare multiple genome assemblies with short-read alignment data" + version
+            description = "Themis-ASM: Compare multiple genome assemblies with short-read alignment data" + version
             )
     parser.add_argument('-a', '--assembly',
                         help="An assembly fasta file to compare. Provide the full path to the file",
@@ -29,9 +35,93 @@ def parse_user_input():
                         help="A sample name for the fastqs provided. Add in the order in which the fastq files are specified",
                         action="append", default=[]
                         )
+    parser.add_argument('-c', '--cluster_string',
+                        help="A short formatted string for instructions on how to submit to your cluster [Default: do not submit to cluster]",
+                        type=str, default=None
+                        )
+    parser.add_argument('-j', '--jobs',
+                        help="Maximum concurrent jobs to run [Default: 20]",
+                        type=int, default=20
+                        )
+    parser.add_argument('-r', '--resume',
+                        help="Resume a previously terminated job [Flag]",
+                        action="store_true", default=False
+                        )
+    parser.add_argument('-u', '--unlock',
+                        help="Unlock a previously terminated job directory [Flag]",
+                        action="store_true", default=False
+                        )
 
     return parser.parse_args(), parser
 
+def main(args, parser):
+    # Sanity checks
+    try:
+        validate(args)
+    except RuntimeError as inst:
+        LOG.error(inst.args)
+        sys.exit()
+
+    # Create the config file
+    try:
+        createConfig(args)
+    except RuntimeError as inst:
+        LOG.error(inst.args)
+        sys.exit()
+
+    # Generate the command
+    cmd = createSNKCmd(args)
+
+    # Now attempt to run it!
+    try:
+        LOG.INFO("Trying to run the snakemake job in this scrpt... wish me luck!")
+        sp.run(cmd, shell=True, check=True)
+    except CalledProcessError as inst:
+        LOG.info("We ran into an error! Please try to resume using this script or run the snake command manually")
+        LOG.error(inst.args)
+        sys.exit()
+
+    LOG.INFO("I think we're finished! Check that zip file!")
+
+def createSNKCmd(args):
+    cmd = f'snakemake -s {SCRIPT_DIR}/themisSnakefile --jobs {args.jobs} -p --use-conda'
+
+    if args.cluster_string != None:
+        LOG.info("Detected cluster submission information! Running cluster jobs")
+        cmd += f' --cluster-config {SCRIPT_DIR}/cluster.json --cluster {args.cluster_string}'
+
+    LOG.info("Created snakemake submission string\nIf you want to run this on your own, please use the following command:\n" + cmd)
+    return cmd
+
+def validate(args):
+    if len(args.name) != len(args.assembly):
+        raise RuntimeError(f'Please enter the same count of assembly fastas {len(args.assembly)} as your names {len(args.name)}')
+    if len(args.sample) != len(args.fastq):
+        raise RuntimeError(f'Please enter the same count of read fastqs {len(args.fastq)} as your sample names {len(args.sample)}')
+
+    rdir = os.path.join(os.getcwd(), '.snakemake')
+    if os.path.isdir(rdir) and not args.resume:
+        raise RuntimeError("It looks like you've already run snakemake in this directory -- please remove or resume your job!")
+    ldir = os.path.join(rdir, 'locks')
+    if os.path.isdir(ldir) and os.listdir(ldir):
+        raise RuntimeError("It looks like the directory is locked by Snakemake. Please run an unlock job first with --unlock!")
+
+def createConfig(args):
+    config = f'{{\n  "buscoLineage" : "{args.busco}",\n  "assembly" : {{\n';
+    for aname, afile in zip(args.name, args.assembly):
+        config += f'    "{aname}" : "{afile}",\n'
+    config += "  },\n  \"samples\" : {\n"
+
+    for sname, fqstr in zip(args.name, args.fastq):
+        fqseg = fqstr.split(',')
+        if len(fqseg) != 2:
+            raise RuntimeError(f'It looks like you entered an unpaired fastq set! Please add both reads as a comma delmited entry!\n')
+        config += f'    "{sname}" : [\n      "{fqseg[0]}",\n      "{fqseg[1]}"\n    ],\n'
+
+    config += "  }\n}\n"
+    with open("default.json", 'w') as co:
+        co.write(config)
+    LOG.info("Created new configuration file: default.json")
 
 if __name__ == "__main__":
     args, parser = parse_user_input()
